c52093d7df433e6574f9c6f3f7e4a7bef7d3cfa9
Geronimo Suarez <geronimosuarez0403@gmail.com>
2025-06-15 06:18:04 -0300

    fix deploy bug

--- src/features/logged/incidents/hooks/use-get-incidents.ts
+++ src/features/logged/incidents/hooks/use-get-incidents.ts
@@ -1,20 +1,20 @@
-// import { useQuery } from '@tanstack/react-query';
-// import { Incident } from '../entities/incident';
-// import { API_URL } from '@/core/api';
-// import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
+import { Incident } from '../entities/incident';
+import { API_URL } from '@/core/api';
+import axios from 'axios';
 import { incidentsMock } from '../mocks/incidents';
 
-// const getIncidents = async (): Promise<Incident[]> => {
-//   const { data } = await axios.get(`${API_URL}/incidents`);
-//   return data;
-// };
+const getIncidents = async (): Promise<Incident[]> => {
+  const { data } = await axios.get(`${API_URL}/incidents`);
+  return data;
+};
 
 export const useGetIncidients = () => {
-  // const { data, isLoading, error } = useQuery({
-  //   queryKey: ['incidents'],
-  //   queryFn: getIncidents,
-  //   // refetchInterval:
-  // });
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['incidents'],
+    queryFn: getIncidents,
+    // refetchInterval:
+  });
 
   // console.log(data);
   // console.log(error);
